e6ebcf724f05ba73ebb7128dd1f3dafe12fce5f8
jamiecraane <jcraane@gmail.com>
2021-01-22 14:33:59 +0100

    Add image to ContentView.swift

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,13 +1,17 @@
 import SwiftUI
+import shared
 
 func greet() -> String {
     return "Images"
 }
 
 struct ContentView: View {
+//   var body: some View {
+//        Text(greet())
+//    }
     var body: some View {
-        Text(greet())
-    }
+             Image(uiImage: UIImage(named: Images().KEYBOARD.name)!)
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- iosApp/iosApp/SceneDelegate.swift
+++ iosApp/iosApp/SceneDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import SwiftUI
+import shared
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -14,10 +15,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
 
+        print("Image = \(Images().KEYBOARD.name)")
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = UIHostingController(rootView: ContentView())
+//            todo add image here
             self.window = window
             window.makeKeyAndVisible()
         }
