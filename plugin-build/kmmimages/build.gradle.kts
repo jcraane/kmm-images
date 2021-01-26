515897493128de553143463bd7f60d308994fe20
jamiecraane <jcraane@gmail.com>
2021-01-26 19:54:01 +0100

    Update version

--- plugin-build/kmmimages/build.gradle.kts
+++ plugin-build/kmmimages/build.gradle.kts
@@ -26,7 +26,7 @@ gradlePlugin {
             id = "dev.jamiecraane.plugins.kmmimages"
             implementationClass = "com.capoax.kmmimages.plugin.KmmImagesPlugin"
             description = description
-            version = "1.0.0-alpha02"
+            version = "1.0.0-alpha03"
         }
     }
 }
