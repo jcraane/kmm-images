fb97063cca947f78f012c9f5a6b6d8c43a03823f
Alexander Lomovskiy <aalomovskii@domclick.ru>
2023-02-25 13:53:57 +0300

    up kotlin gradle plugin version to 1.7.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.0")
         classpath("com.github.jengelman.gradle.plugins:shadow:2.0.2")
-        classpath(kotlin("gradle-plugin", version = "1.6.0"))
+        classpath(kotlin("gradle-plugin", version = "1.7.21"))
     }
 }
 
